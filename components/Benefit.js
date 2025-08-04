daef34c1c4a5397bb2572e14617d4a37733ed8b2
Rad <chihaby@gmail.com>
2025-08-04 06:55:12 -0700

    updates landing page

--- components/Benefit.js
+++ components/Benefit.js
@@ -1,17 +1,17 @@
-import styles from "../styles/Benefits.module.css";
-import Image from "next/image";
+import styles from '../styles/Benefits.module.css';
+import Image from 'next/image';
 // import design from "../public/assets/svg/design.svg";
-import ux from "../public/assets/svg/ux.svg";
-import performance from "../public/assets/svg/performance.svg";
-import axe from "../public/assets/svg/axe.svg";
+import ux from '../public/assets/svg/ux.svg';
+import performance from '../public/assets/svg/performance.svg';
+import axe from '../public/assets/svg/axe.svg';
 // import copy from '../public/assets/svg/copy.svg';
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const Benefit = () => {
   return (
     // <div style={{ backgroundColor: "#E0E8E7" }}>
-    <div style={{ backgroundColor: "#FFFFFF" }}>
+    <div style={{ backgroundColor: '#FAFAFA' }}>
       <div className={styles.container}>
         <h2 className={styles.title}>{"We've"} Got the Remedy!</h2>
         <br />
@@ -27,42 +27,17 @@ const Benefit = () => {
             action.
           </Col>
         </Row> */}
-        <Row style={{ padding: "16px" }}>
+        <Row style={{ padding: '16px' }}>
           <Col sm={12} md={4} lg={4}>
             <div className={styles.icon}>
-              <Image
-                src={performance}
-                alt="performance"
-                width={200}
-                height={150}
-              />
+              <Image src={ux} alt='performance' width={200} height={150} />
             </div>
             <div
               className={styles.item_title}
               style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
-              }}
-            >
-              UI/UX
-            </div>
-            <br />
-            <div className={styles.description}>
-              Higher engagements, better search engine ranking, increased
-              conversions, and reduced bounce rates.
-            </div>
-          </Col>
-          <Col sm={12} md={4} lg={4}>
-            <div className={styles.icon}>
-              <Image src={ux} alt="performance" width={200} height={150} />
-            </div>
-            <div
-              className={styles.item_title}
-              style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
               }}
             >
               Design & Development
@@ -75,31 +50,9 @@ const Benefit = () => {
           </Col>
           <Col sm={12} md={4} lg={4}>
             <div className={styles.icon}>
-              <Image src={axe} alt="features" width={200} height={150} />
-            </div>
-            <div
-              className={styles.item_title}
-              style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
-              }}
-            >
-              Accessibility
-            </div>
-            <br />
-            <div className={styles.description}>
-              100% compliant with the legal requirements and accessibility
-              standards {"(WCAG)"}
-            </div>
-          </Col>
-        </Row>
-        <Row style={{ padding: "16px" }}>
-          <Col sm={12} md={4} lg={4}>
-            <div className={styles.icon}>
               <Image
                 src={performance}
-                alt="performance"
+                alt='performance'
                 width={200}
                 height={150}
               />
@@ -107,9 +60,56 @@ const Benefit = () => {
             <div
               className={styles.item_title}
               style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
+              }}
+            >
+              UI/UX
+            </div>
+            <br />
+            <div className={styles.description}>
+              Higher engagements, better search engine ranking, increased
+              conversions, and reduced bounce rates.
+            </div>
+          </Col>
+          <Col sm={12} md={4} lg={4}>
+            <div className={styles.icon}>
+              <Image src={axe} alt='features' width={200} height={150} />
+            </div>
+            <div
+              className={styles.item_title}
+              style={{
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
+              }}
+            >
+              Accessibility
+            </div>
+            <br />
+            <div className={styles.description}>
+              100% compliant with the legal requirements and accessibility
+              standards {'(WCAG)'}
+            </div>
+          </Col>
+        </Row>
+        <Row style={{ padding: '16px' }}>
+          <Col sm={12} md={4} lg={4}>
+            <div className={styles.icon}>
+              <Image
+                src={performance}
+                alt='performance'
+                width={200}
+                height={150}
+              />
+            </div>
+            <div
+              className={styles.item_title}
+              style={{
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
               }}
             >
               Writing for the WEB / Copy
@@ -122,14 +122,14 @@ const Benefit = () => {
           </Col>
           <Col sm={12} md={4} lg={4}>
             <div className={styles.icon}>
-              <Image src={ux} alt="performance" width={200} height={150} />
+              <Image src={ux} alt='performance' width={200} height={150} />
             </div>
             <div
               className={styles.item_title}
               style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
               }}
             >
               SEO
@@ -142,14 +142,14 @@ const Benefit = () => {
           </Col>
           <Col sm={12} md={4} lg={4}>
             <div className={styles.icon}>
-              <Image src={axe} alt="features" width={200} height={150} />
+              <Image src={axe} alt='features' width={200} height={150} />
             </div>
             <div
               className={styles.item_title}
               style={{
-                fontSize: "24px",
-                fontWeight: "600",
-                textAlign: "center",
+                fontSize: '24px',
+                fontWeight: '600',
+                textAlign: 'center',
               }}
             >
               Logo Design
@@ -157,7 +157,7 @@ const Benefit = () => {
             <br />
             <div className={styles.description}>
               100% compliant with the legal requirements and accessibility
-              standards {"(WCAG)"}
+              standards {'(WCAG)'}
             </div>
           </Col>
         </Row>
