ed453388ba59b1dab4af10174ed92af5c9c8a918
Radouane Chihaby <chihaby@gmail.com>
2025-08-18 15:03:45 -0700

    edits robot file locaiton

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.mywebsite.com",
+      url: "https://www.bayareawebdesign.net",
       lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 1,
     },
     {
-      url: "https://www.mywebsite.com/services",
+      url: "https://www.bayareawebdesign.net/services",
       lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.8,
