6ec7f961052d5c6c44ce880ac67f4ce6872dac39
Radouane Chihaby <chihaby@gmail.com>
2025-08-21 12:13:01 -0700

    adds link instead of a tag

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 // app/not-found.tsx
 export default function NotFound() {
   return (
     <div className="container">
       <h1>404</h1>
       <p>Oops! The page you’re looking for doesn’t exist.</p>
-      <a href="/" className="button">
+      <Link href="/" className="button">
         Back to Home
-      </a>
+      </Link>
     </div>
   );
 }
