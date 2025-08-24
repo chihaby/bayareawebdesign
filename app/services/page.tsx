010139a3c008e4a80f7b886f7e2847a0053a489b
Radouane Chihaby <chihaby@gmail.com>
2025-08-24 04:12:31 -0700

    edits servixes page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -25,9 +25,9 @@ const services = () => {
           <hr />
           <br />
           <br />
+
           <Row className="justify-content-md-center">
             <Col sm={12} md={6} lg={6}>
-              <b>Auto</b>
               <a
                 href="https://www.blitzdetail.com/"
                 target="_blank"
@@ -51,6 +51,35 @@ const services = () => {
                   </Card>
                 </div>
               </a>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "100px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
+            </Col>
+          </Row>
+          <br />
+          <br />
+
+          <br />
+          <br />
+          <Row className="justify-content-md-center">
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
             </Col>
             <Col sm={12} md={6} lg={6}>
               <b>Web Content Accessibility WCAG</b>
@@ -79,6 +108,57 @@ const services = () => {
           <br />
           <Row>
             <Col sm={12} md={6} lg={6}>
+              <b>Coffee Shop</b>
+              <a
+                href="https://cafe-lisbon-2o6tx7f31-rads-projects-304e987f.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "#444444" }}
+              >
+                <div>
+                  <Card>
+                    <Image src={cafelisbon} alt="Cafe Lisbon" />
+                    <Card.Body>
+                      <Card.Title>Cafe Lisbon</Card.Title>
+                      <Card.Text>
+                        Hand picked artisan coffee, locally sourced ingredients,
+                        ensuring premium quality.
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Body></Card.Body>
+                  </Card>
+                </div>
+              </a>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
+            </Col>
+          </Row>
+          <br />
+          <br />
+          <br />
+          <br />
+          <Row>
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
               <b>Pizzeria</b>
               <a
                 href="https://downtownpizza.netlify.app/"
@@ -104,56 +184,10 @@ const services = () => {
                 </div>
               </a>
             </Col>
-            <Col sm={12} md={6} lg={6}>
-              <b>Coffee Shop</b>
-              <a
-                href="https://cafe-lisbon-2o6tx7f31-rads-projects-304e987f.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none", color: "#444444" }}
-              >
-                <div>
-                  <Card>
-                    <Image src={cafelisbon} alt="Cafe Lisbon" />
-                    <Card.Body>
-                      <Card.Title>Cafe Lisbon</Card.Title>
-                      <Card.Text>
-                        Hand picked artisan coffee, locally sourced ingredients,
-                        ensuring premium quality.
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Body></Card.Body>
-                  </Card>
-                </div>
-              </a>
-            </Col>
           </Row>
           <br />
           <br />
           <Row className="justify-content-md-center">
-            <Col sm={12} md={6} lg={6}>
-              <b>Entrepreneur</b>
-              <a
-                href="https://client-custom-website.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none", color: "#444444" }}
-              >
-                <div>
-                  <Card>
-                    <Image src={about} alt="portfolio website" />
-                    <Card.Body>
-                      <Card.Title>Portfolio</Card.Title>
-                      <Card.Text>
-                        The portfolio model is for professionals who want to
-                        showcase their skills and services.
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Body></Card.Body>
-                  </Card>
-                </div>
-              </a>
-            </Col>
             <Col sm={12} md={6} lg={6}>
               <b>Music</b>
               <a
@@ -170,6 +204,53 @@ const services = () => {
                       <Card.Text>
                         The band model is for artists who want to showcase their
                         work and links to where to buy.
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Body></Card.Body>
+                  </Card>
+                </div>
+              </a>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col sm={12} md={6} lg={6}>
+              <Card.Text style={{ marginTop: "200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                in lorem porta, accumsan elit sed, rhoncus eros. Sed sed gravida
+                odio. Nulla non accumsan justo, vitae tempus nibh. Duis varius
+                viverra purus quis porttitor. Duis ultricies in est vitae
+                sollicitudin. Vivamus porta erat at ipsum rhoncus, a lacinia mi
+                vestibulum. Nulla interdum nisl non laoreet tristique.
+                Suspendisse semper sit amet nisi ac mattis..
+              </Card.Text>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <b>Entrepreneur</b>
+              <a
+                href="https://client-custom-website.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "#444444" }}
+              >
+                <div>
+                  <Card>
+                    <Image src={about} alt="portfolio website" />
+                    <Card.Body>
+                      <Card.Title>Portfolio</Card.Title>
+                      <Card.Text>
+                        The portfolio model is for professionals who want to
+                        showcase their skills and services.
                       </Card.Text>
                     </Card.Body>
                     <Card.Body></Card.Body>
